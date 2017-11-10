de469d0082a69d892872df0152572d5815f8f906
track11 <359063632@qq.com>
2017-11-10 10:33:57 +0800

    初始化

---
+++ tuition/src/main/java/com/eshore/string/Test.java
@@ -0,0 +1,11 @@
+package com.eshore.string;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer a = new Integer(3);
+        Integer b = 3;                  // 将3自动装箱成Integer类型
+        int c = 3;
+        System.out.println(a == b);     // false 两个引用没有引用同一对象
+        System.out.println(a == c);
+    }
+}
